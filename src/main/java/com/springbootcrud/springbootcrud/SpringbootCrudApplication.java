c4c897a590c39c85a2b1a8aa5fee417b48833e66
Meet Patel <meet1999.patel@gmail.com>
2022-01-05 17:16:16 +0530

    .

--- src/main/java/com/springbootcrud/springbootcrud/SpringbootCrudApplication.java
+++ src/main/java/com/springbootcrud/springbootcrud/SpringbootCrudApplication.java
@@ -1,5 +1,7 @@
 package com.springbootcrud.springbootcrud;
 
+
+import org.sonar.wsclient.SonarClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,15 @@ public class SpringbootCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCrudApplication.class, args);
+        SonarClient sonarClient=SonarClient.builder()
+                .url("http://localhost:9000")
+                .login("admin")
+                .password("Interns@1234")
+                .build();
+        String sonarEndpoint="/api/webservices/list";
+
+        String sonarResponse=sonarClient.get(sonarEndpoint);
+        System.out.println(sonarResponse);
     }
 
 }
